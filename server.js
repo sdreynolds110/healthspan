240a5550f603f40eb8d812d98b48506f0f67171d
Stephen Reynolds <stephenreynolds@Stephens-MacBook-Pro.local>
2020-04-25 10:47:30 -0400

    added static express

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const routes = require("./routes/api");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/", express.static(path.join(_dirname, "/client/build")));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
